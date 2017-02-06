71662a9626cd32a7e06e3df53a6ad14761ae085f
ohager <oliver.hager@dextra.com.br>
2017-02-06 13:39:16 -0200

    Introduced simple state history

--- src/DevTools.js
+++ src/DevTools.js
@@ -1,27 +1,43 @@
 import React, {Component, createFactory} from 'react';
+import _ from 'lodash';
 import JsonInspector from 'react-json-inspector';
 import JsonInspectorValueEditor from './JsonInspectorValueEditor'
 
 import './css/JsonInspector.css';
 import './css/DevTools.css';
 
+const DevToolsStateEditor = (props) => (
+	<div className="devtools-state-editor">
+		<JsonInspector data={props.state || {}} search={false} interactiveLabel={props.editorFactory}/>
+	</div>
+);
 
-class DevToolsStateItem extends Component {
-	render() {
-		return (
+DevToolsStateEditor.propTypes = {
+	state : React.PropTypes.object.isRequired,
+	editorFactory : React.PropTypes.func.isRequired
+};
 
-			<div>
-				<JsonInspector data={this.props.storeState || {}} search={false} interactiveLabel={this.props.editorFactory}/>
-			</div>
-		)
-	}
-}
+const DevToolsStateItem = (props) => {
+	return	(
+		<div className={`devtools-state-item ${props.selected ? 'selected' : ''}`} onClick={() => {props.onClick(props.age, props.state)} }>
+			<p>{`State - age ${props.age}`}</p>
+		</div>
+	)
+};
+
+DevToolsStateItem.propTypes = {
+	onClick : React.PropTypes.func.isRequired,
+	age : React.PropTypes.number.isRequired,
+	state : React.PropTypes.object.isRequired,
+	selected : React.PropTypes.bool
+};
 
 class DevTools extends Component {
 
 	constructor() {
 		super();
-		this.state = {};
+		this.state = { history: [], currentStateIndex: 0 };
+		this.onHistoryItemClicked = this.onHistoryItemClicked.bind(this);
 	}
 
 	getChildContext(){
@@ -29,7 +45,9 @@ class DevTools extends Component {
 	}
 
 	onStoreUpdate(state) {
-		this.setState({storeState: state});
+		let history = this.state.history;
+		history.unshift(_.cloneDeep(state));
+		this.setState({history: history});
 	}
 
 	componentWillMount() {
@@ -41,18 +59,32 @@ class DevTools extends Component {
 		this.props.storeAdapter.unmount();
 	}
 
-	renderStateItems() {
-		//const stateProps = Object.keys(this.state.storeState);
-		//return stateProps.map( (state, index) => <DevToolsStateItem key={index}/>)
-		return <DevToolsStateItem storeState={this.state.storeState} editorFactory={this.valueEditorFactory}/>
+	onHistoryItemClicked(index, state) {
+		this.setState({currentStateIndex : index}); // can I use reference?! Why not? Need to see!
+	}
+
+	renderHistory() {
+		const items =  _.map(this.state.history, (state, index) => <DevToolsStateItem
+			key={index}
+			age={index}
+			onClick={this.onHistoryItemClicked}
+			state={state}
+			selected={index === this.state.currentStateIndex} />);
+
+		return ( <div className="devtools-history">{items}</div> )
 	}
 
 	render() {
+
+		const currentState = this.state.history[this.state.currentStateIndex] || {};
+
 		return (
 			<div className="devtools-container">
 				<h2>Flux DevTools</h2>
 				<hr/>
-				{ this.renderStateItems() }
+				<DevToolsStateEditor state={currentState} editorFactory={this.valueEditorFactory}/>
+				<hr/>
+				{this.renderHistory()}
 			</div>
 		);
 	}
